99ce5d2e1942a6aeb48e731ad021184c44439582
IPIvliev <cccp1945@mail.ru>
2022-06-03 13:20:14 +0300

    Changes with static

--- dorogaminina_django/settings.py
+++ dorogaminina_django/settings.py
@@ -16,8 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-STATIC_URL = '/static/'
 STATICFILE_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     STATICFILE_DIR,
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
