38ffb9f34c3618fe6a07a412e767394eb42bf8dc
IPIvliev <cccp1945@mail.ru>
2022-06-08 17:52:39 +0300

    Add payments

--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.dispatch import receiver
 from events.models import Order
+from home.views import index
 
 from django.http import HttpResponse
 from robokassa.signals import result_received, success_page_visited, fail_page_visited
@@ -10,16 +11,16 @@ def payment_received(sender, **kwargs):
   order.active = True
   order.price = kwargs['OutSum']
   order.save()
-  return HttpResponse('<h1>Hello HttpResponse</h1>')
+  return redirect(index)
 
 result_received.connect(payment_received)
 
 def payment_success(sender, **kwargs):
-  return HttpResponse('<h1>Paiment Success</h1>')
+  return redirect(index)
 
 success_page_visited.connect(payment_success)
 
 def payment_fail(sender, **kwargs):
-  return HttpResponse('<h1>Paiment Fail</h1>')
+  return redirect(index)
 
 fail_page_visited.connect(payment_fail)
