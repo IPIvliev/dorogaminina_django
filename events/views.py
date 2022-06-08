3a9cf790443c0e8a1a395d22e122c54f07a20d52
IPIvliev <cccp1945@mail.ru>
2022-06-08 18:41:50 +0300

    Add payments

--- events/views.py
+++ events/views.py
@@ -1,21 +1,8 @@
 from django.shortcuts import render, redirect
-from django.dispatch import receiver
 from events.models import Order
-from home.views import index
+from home.views import profile
 
-from django.http import HttpResponse
-from robokassa.signals import result_received, success_page_visited, fail_page_visited
-from robokassa.forms import ResultURLForm, SuccessRedirectForm, FailRedirectForm
 from django.views.decorators.csrf import csrf_exempt
-
-@csrf_exempt
-def payment_received(request, sender, **kwargs):
-  print(kwargs)
-  order = Order.objects.get(id=kwargs['InvId'])
-  order.active = True
-  order.price = kwargs['OutSum']
-  order.save()
-  return redirect(index)
 
 @csrf_exempt
 def payment_success(request):
@@ -24,10 +11,9 @@ def payment_success(request):
   out_sum = data.get("OutSum")
   order = Order.objects.get(id=inv_id)
   order.active = True
-  order.price = int(out_sum)
   order.save()
-  return redirect(index)
+  return redirect(profile)
 
 @csrf_exempt
 def payment_fail(sender, **kwargs):
-  return redirect(index)
+  return redirect(profile)
