8d90fd3a9d67346e3e84cd48b0f6d461289cdb7b
IPIvliev <cccp1945@mail.ru>
2022-06-08 18:45:01 +0300

    Add payments

--- dorogaminina_django/urls.py
+++ dorogaminina_django/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from home import views
-from events.views import payment_success, payment_fail, payment_received
+from events.views import payment_success, payment_fail
 
 urlpatterns = [
   path('', views.index),
