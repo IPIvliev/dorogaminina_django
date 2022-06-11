17be4d944fe39d2bdb3aa47ee2e894a505b72cfc
IPIvliev <cccp1945@mail.ru>
2022-06-11 13:13:25 +0300

    Add filter and export for Orders

--- events/admin.py
+++ events/admin.py
@@ -14,9 +14,13 @@ class OrderResource(resources.ModelResource):
 @admin.register(Order)
 class OrderAdmin(ExportActionMixin, admin.ModelAdmin):
     resource_class = OrderResource
-    list_display = ('order_user', 'order_event', 'order_place', 'order_merch', 'price', 'active')
-    list_filter = ('active',)
+    list_display = ('order_user', 'get_user', 'order_place', 'order_merch', 'price', 'active')
+    list_filter = ('active', 'order_event__event_name')
     search_fields = ['order_user__username', 'order_user__lastname', 'order_user__middlename', 'order_user__phone']
+
+    @admin.display(description='User')
+    def get_user(self, obj):
+        return '%s %s' % (obj.order_user.lastname, obj.order_user.username)
 
 class EventAdminForm(forms.ModelForm):
     about = forms.CharField(widget=CKEditorWidget())
