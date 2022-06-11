b86f0f993140c8d4df8397ab13e82aec9de598b9
IPIvliev <cccp1945@mail.ru>
2022-06-11 13:58:28 +0300

    Add filter and export for Orders

--- dorogaminina_django/settings.py
+++ dorogaminina_django/settings.py
@@ -18,13 +18,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 STATICFILE_DIR = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 
-# STATICFILES_DIRS = [
-#     STATICFILE_DIR,
-# ]
+STATICFILES_DIRS = [
+    STATICFILE_DIR,
+]
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 MEDIA_URL= "/media/"
