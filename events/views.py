1e4fdc2dd4ea6eb98f3c4c0ff7205f75d48789e2
IPIvliev <cccp1945@mail.ru>
2022-06-19 12:35:28 +0300

    Error

--- events/views.py
+++ events/views.py
@@ -4,14 +4,14 @@ from home.views import profile
 
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
-def payment_received(request):
-  data = request.POST
-  inv_id = data.get("InvId")
-  order = Order.objects.get(id=inv_id)
-  order.active = True
-  order.save()
-  return redirect(profile)
+# @csrf_exempt
+# def payment_received(request):
+#   data = request.POST
+#   inv_id = data.get("InvId")
+#   order = Order.objects.get(id=inv_id)
+#   order.active = True
+#   order.save()
+#   return redirect(profile)
 
 @csrf_exempt
 def payment_success(request):
