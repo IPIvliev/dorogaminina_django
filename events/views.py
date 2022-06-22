71fdc2db6b7cc121e41eeaa0f7d56caaf2bf6a09
IPIvliev <cccp1945@mail.ru>
2022-06-22 09:16:45 +0300

    Change robokassa

--- events/views.py
+++ events/views.py
@@ -6,11 +6,20 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def payment_received(request):
-  pass
+  data = request.POST
+  print("Request from payment_received")
+  print(data)
+  inv_id = data.get("InvId")
+  order = Order.objects.get(id=inv_id)
+  order.active = True
+  order.save()
+  return redirect(profile)
 
 @csrf_exempt
 def payment_success(request):
   data = request.POST
+  print("Request from payment_success")
+  print(data)
   inv_id = data.get("InvId")
   order = Order.objects.get(id=inv_id)
   order.active = True
